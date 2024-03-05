b27930758b6447acc85152bd97c3c4ae99a1b34c
luogen1996 <38316576+luogen1996@users.noreply.github.com>
2024-03-05 19:02:46 +0800

    update

--- scripts/v1_5/train_eval_llava_hr.sh
+++ scripts/v1_5/train_eval_llava_hr.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-deepspeed llava_hr/train/train_mem.py \
-    --deepspeed ./scripts/zero2.json \
-    --model_name_or_path /data/vicuna/vicuna-7b-v1.5 \
-    --version plain \
-    --data_path /data/data/blip_laion_cc_sbu_558k.json \
-    --image_folder /data/data/images \
-    --vision_tower openai/clip-vit-large-patch14-336 \
-    --vision_tower_slow convnext_large_mlp.clip_laion2b_ft_320 \
-    --mm_projector_type mlp2x_gelu \
-    --tune_mm_mlp_adapter True \
-    --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-hr-7b-pretrain-384 \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 24000 \
-    --save_total_limit 1 \
-    --learning_rate 1e-3 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --tf32 True \
-    --model_max_length 2048 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
-    --lazy_preprocess True \
-    --report_to wandb \
-    --is_multipath_encoder True \
-    --input_image_size 384
-
 
 deepspeed llava_hr/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
@@ -54,7 +17,7 @@ deepspeed llava_hr/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-hr-7b-sft-768 \
+    --output_dir ./checkpoints/llava-hr-7b-sft-1024 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
@@ -78,4 +41,4 @@ deepspeed llava_hr/train/train_mem.py \
     --freeze_vision False \
     --input_image_size 1024
 
-bash scripts/v1_5/eval.sh ./checkpoints/llava-hr-7b-sft-768 2>&1 | tee log-llava-hr-7b-sft-768.txt
+bash scripts/v1_5/eval.sh ./checkpoints/llava-hr-7b-sft-1024 2>&1 | tee log-llava-hr-7b-sft-1024.txt
--- scripts/v1_5/train_eval_llava_hr_x.sh
+++ scripts/v1_5/train_eval_llava_hr_x.sh
@@ -1,52 +1,17 @@
 #!/bin/bash
 
-deepspeed llava_hr/train/train_mem.py \
-    --deepspeed ./scripts/zero2.json \
-    --model_name_or_path /data/vicuna/vicuna-7b-v1.5 \
-    --version plain \
-    --data_path /data/data/blip_laion_cc_sbu_558k.json \
-    --image_folder /data/data/images \
-    --vision_tower openai/clip-vit-large-patch14-336 \
-    --vision_tower_slow convnext_xxlarge.clip_laion2b_soup \
-    --mm_projector_type mlp2x_gelu \
-    --tune_mm_mlp_adapter True \
-    --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-hr-x-7b-pretrain-384 \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 24000 \
-    --save_total_limit 1 \
-    --learning_rate 1e-3 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --tf32 True \
-    --model_max_length 2048 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
-    --lazy_preprocess True \
-    --report_to wandb \
-    --is_multipath_encoder True \
-    --input_image_size 384
+
 
 
 deepspeed llava_hr/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path /data/vicuna/vicuna-7b-v1.5 \
+    --model_name_or_path /data/vicuna/vicuna-13b-v1.5 \
     --version v1 \
     --data_path ./playground/data/llava_v1_5_mix665k.json \
     --image_folder ./playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --vision_tower_slow convnext_xxlarge.clip_laion2b_soup \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-hr-x-7b-pretrain-384/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./checkpoints/llava-hr-x-13b-pretrain-384/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -54,7 +19,7 @@ deepspeed llava_hr/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-hr-x-7b-sft-768 \
+    --output_dir ./checkpoints/llava-hr-x-13b-sft-768 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
@@ -78,4 +43,4 @@ deepspeed llava_hr/train/train_mem.py \
     --freeze_vision False \
     --input_image_size 1024
 
-bash scripts/v1_5/eval.sh ./checkpoints/llava-hr-x-7b-sft-768 2>&1 | tee log-llava-hr-x-7b-sft-768.txt
+bash scripts/v1_5/eval.sh ./checkpoints/llava-hr-x-13b-sft-1024 2>&1 | tee log-llava-hr-x-13b-sft-1024.txt
